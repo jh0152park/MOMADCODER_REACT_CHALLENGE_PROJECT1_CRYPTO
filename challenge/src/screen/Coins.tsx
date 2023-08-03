a485d8f54c3fe54e060cfe0c88833f95c5d1eafe
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-03 18:15:16 +0900

    added feature to get coin list from api

--- challenge/src/screen/Coins.tsx
+++ challenge/src/screen/Coins.tsx
@@ -1,96 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import { styled } from "styled-components";
+import {
+    Coin,
+    CoinList,
+    Header,
+    Loading,
+    Title,
+    Wrapper,
+} from "./style/CoinsStyle";
+import { ICoin } from "./types/CoinsType";
 
-const Wrapper = styled.div`
-    padding: 0px 20px;
-`;
-
-const Header = styled.header`
-    height: 10vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 50px;
-    font-weight: bold;
-`;
-
-const CoinList = styled.ul`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`;
-
-const Coin = styled.li`
-    width: 300px;
-    height: 50px;
-    border-radius: 10px;
-    border: 1px solid ${(props) => props.theme.accentColor};
-    margin-bottom: 20px;
-
-    a {
-        padding: 15px;
-        display: block;
-        transition: color 0.2s ease-out;
-    }
-
-    &:hover {
-        /* cursor: pointer; */
-        a {
-            color: ${(props) => props.theme.accentColor};
-        }
-    }
-`;
-
-const Title = styled.h1`
-    color: ${(props) => props.theme.accentColor};
-`;
-
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-];
+const URL = "https://api.coinpaprika.com/v1/coins";
 
 function Coins() {
+    const [coins, setCoins] = useState<ICoin[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    async function getCoinList() {
+        const response = await axios.get(URL);
+        setCoins(response.data.slice(0, 100));
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getCoinList();
+    }, []);
+
     return (
         <Wrapper>
             <Header>
                 <Title>Coin</Title>
             </Header>
 
-            <CoinList>
-                {coins.map((coin) => (
-                    <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} ➡</Link>
-                    </Coin>
-                ))}
-            </CoinList>
+            {loading ? (
+                <Loading>Loading...</Loading>
+            ) : (
+                <CoinList>
+                    {coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`}>{coin.name} ➡</Link>
+                        </Coin>
+                    ))}
+                </CoinList>
+            )}
         </Wrapper>
     );
 }
---
+++ challenge/src/screen/style/CoinsStyle.tsx
@@ -0,0 +1,54 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+    padding: 0px 20px;
+`;
+
+export const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 50px;
+    font-weight: bold;
+`;
+
+export const CoinList = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const Coin = styled.li`
+    width: 300px;
+    height: 50px;
+    border-radius: 10px;
+    border: 1px solid ${(props) => props.theme.accentColor};
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+
+    a {
+        padding: 15px;
+        display: block;
+        transition: color 0.2s ease-out;
+    }
+
+    &:hover {
+        /* cursor: pointer; */
+        a {
+            color: ${(props) => props.theme.accentColor};
+        }
+    }
+`;
+
+export const Title = styled.h1`
+    color: ${(props) => props.theme.accentColor};
+`;
+
+export const Loading = styled.span`
+    text-align: center;
+    font-size: 20px;
+    display: block;
+`;
